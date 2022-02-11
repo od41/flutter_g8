db07f0d5c9e374de2070ea41958b41cbcf4e7db2
Odafe <odafe41@gmail.com>
2022-02-11 00:30:57 +0000

    first working commit

---
+++ android/app/src/main/kotlin/com/example/flutter_g8/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_g8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/my_app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_g8/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Home Page',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
